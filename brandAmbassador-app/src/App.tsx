2a681be139d041f23173e87c8100199baaaa894c
Dcoveney22 <156584156+Dcoveney22@users.noreply.github.com>
2025-03-16 12:37:25 +0000

    removed more bumpff

--- brandAmbassador-app/src/App.tsx
+++ brandAmbassador-app/src/App.tsx
@@ -1,10 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 async function App() {
-  const [count, setCount] = useState(0);
   const response = await fetch("http://localhost:3000/stockSheet");
   const data = await response.json();
   console.log(data);
